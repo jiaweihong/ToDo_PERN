00abe45577b1fe49cd37b0f32c420f06144ec161
jiaweihong <jiaweihong1@hotmail.com>
2020-05-22 15:25:25 +0800

    provided link to serve static files

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 
 // Loads static files from the build file
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client', 'build')))
+	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 }
 
 // Create an account
