d493e9b9dba03b26e3ec28f7d9f00e3e84eb5a1a
jiaweihong <jiaweihong1@hotmail.com>
2020-05-23 22:12:23 +0800

    index

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post('/account', async (req, res) => {
 
 // Sign in with JWT
 app.post('/account/authenticate', async (req, res) => {
-	return res.status(200).send(jwtKey).end()
+	return res.send(jwtKey)
 	try {
 		const { username, password } = req.body
 
